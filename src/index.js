a27c0a7e23ed8f58b414db87dbd34cee6a30bb96
ravneet <ravneetkaur91997@gmail.com>
2021-10-22 14:30:32 +0530

    Set-up server app and logging

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+const config = require('./config/config');
+const logger = require('./config/logger');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.listen(config.port , () => {
+    logger.info(`Listening to port ${config.port}`);
+})
